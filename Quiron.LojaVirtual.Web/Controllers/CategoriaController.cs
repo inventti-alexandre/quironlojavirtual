979e70f7e3680225cd18f2404bf521356188ed03
jbalessandro <jb.alessandro@gmail.com>
2015-05-24 19:22:31 -0300

    Menu categoria

---
+++ Quiron.LojaVirtual.Web/Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private ProdutoRespositorio _repositorio;
+
+        // GET: Categoria
+        public PartialViewResult Menu(string categoria = null)
+        {
+            // instancia repositorio de produtos
+            _repositorio = new ProdutoRespositorio();
+
+            // lista de categorias
+            IEnumerable<string> categorias = _repositorio.Produtos
+                .Select(c => c.Categoria)
+                .Distinct()
+                .OrderBy(c => c);
+
+            // ViewBag
+            ViewBag.CategoriaSelecionada = categoria;
+
+            // retorna Menu view com IEnumerable de categorias
+            return PartialView(categorias);
+        }
+    }
+}
