6e11ebbab1e6d280d056840c6b1ebcf99389b30a
jbalessandro <jb.alessandro@gmail.com>
2015-05-24 22:22:04 -0300

    Carrinho - UnitTest

--- Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
+++ Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -35,6 +37,133 @@ namespace Quiron.LojaVirtual.UnitTest
             Assert.AreEqual(itens.Length, 2);
             Assert.AreEqual(itens[0].Produto, produto1);
             Assert.AreEqual(itens[1].Produto, produto2);
+        }
+
+        [TestMethod]
+        public void AdicionarProdutoExistenteAoCarrinho()
+        {
+            // Arrange - criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+            };
+
+            // Arrange - adiciona itens ao carrinho
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 10);
+
+            // Act
+            ItemCarrinho[] resultado = carrinho.ItensCarrinho.OrderBy(c => c.Produto.ProdutoId).ToArray();
+
+            // Assert
+            Assert.AreEqual(resultado.Length, 2);
+            Assert.AreEqual(resultado[0].Quantidade, 11);
+            Assert.AreEqual(resultado[1].Quantidade, 1);
+        }
+
+        [TestMethod]
+        public void RemoverItensCarrinho()
+        {
+            // Arrange - criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+            };
+
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3",
+            };
+
+            // Arrange - adiciona itens ao carrinho
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto3, 5);
+            carrinho.AdicionarItem(produto2, 1);
+
+            // Act
+            carrinho.RemoverItem(produto2);
+
+            // Assert
+            Assert.AreEqual(carrinho.ItensCarrinho.Where(c => c.Produto == produto2).Count(), 0);
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 2);
+        }
+
+        [TestMethod]
+        public void CalcularValorTotal()
+        {
+            // Arrange - criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 3);
+
+            // Act
+            decimal resultado = carrinho.ObterValorTotal();
+
+            // Assert
+            Assert.AreEqual(resultado, 450);
+        }
+
+        [TestMethod]
+        public void LimparItensCarrinho()
+        {
+            // Arrange - criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+
+            // Act
+            carrinho.LimparCarrinho();
+
+            // Assert
+            Assert.AreEqual(carrinho.ItensCarrinho.Count, 0);
         }
     }
 }
