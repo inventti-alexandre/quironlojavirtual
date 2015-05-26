18f70bc3a90fbca3951f7c61014d2be0d3b87249
jalessandro <jb.alessandro@gmail.com>
2015-05-26 19:56:44 -0300

    Adm - Produto

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 
 namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
 {
     public class ProdutoController : Controller
     {
+
+        private ProdutoRespositorio _repositorio;
+
         // GET: Administrativo/Produto
         public ActionResult Index()
         {
-            return View();
+            _repositorio = new ProdutoRespositorio();
+
+            var produtos = _repositorio.Produtos.OrderBy(p => p.Nome);
+
+            return View(produtos);
         }
     }
 }
