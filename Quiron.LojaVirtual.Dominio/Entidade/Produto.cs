fd6cdeaba7ebc5046148d7352b60bd8aa9101539
jbalessandro <jb.alessandro@gmail.com>
2015-05-26 21:26:00 -0300

    Alterando Produtos

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quiron.LojaVirtual.Dominio.Entidade
 {
     public class Produto
     {
+        [HiddenInput(DisplayValue=false)]
         public int ProdutoId { get; set; }
+        
         public string Nome { get; set; }
+        
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Dominio.Entidade;
 
 namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
 {
@@ -20,6 +21,22 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
             var produtos = _repositorio.Produtos.OrderBy(p => p.Nome);
 
             return View(produtos);
+        }
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new ProdutoRespositorio();
+
+            var produto = _repositorio.Produtos
+                .FirstOrDefault(x => x.ProdutoId == produtoId);
+
+            return View(produto);
+        }
+
+        [HttpPost]
+        public ViewResult Alterar(Produto produto)
+        {
+            return View(produto);
         }
     }
 }
